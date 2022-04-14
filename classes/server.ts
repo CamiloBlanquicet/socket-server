155dc8a19c6227c82f824e6f5b8993ab9127f8fb
CamiloBlanquicet <blanquicetcar@gmail.com>
2022-04-14 14:36:30 -0500

    Servidor para graficas

--- classes/server.ts
+++ classes/server.ts
@@ -1,46 +1,71 @@
+
 import express from 'express';
-import { SERVER_PORT } from '../global/enviroment';
+import { SERVER_PORT } from '../global/environment';
 import socketIO from 'socket.io';
 import http from 'http';
+
 import * as socket from '../sockets/socket';
 
-//aplico el patron singleton para instanciar una sola vez a server
-export default class Server{
-    private static _instance: Server;
-    public app:express.Application;
+ 
+
+export default class Server {
+
+    private static _intance: Server;
+
+    public app: express.Application;
     public port: number;
-    public io: socketIO.Server;//encargado de los eventos
+
+    public io: socketIO.Server;
     private httpServer: http.Server;
-    private constructor(){
-        this.app=express();
-        this.port=SERVER_PORT;
-        this.httpServer=new http.Server(this.app);
-        this.io=require("socket.io")(this.httpServer, {
-            cors: {
-                origin: true,
-                credentials: true
-              },            
-          });
+
+
+    private constructor() {
+
+        this.app = express();
+        this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
+
         this.escucharSockets();
     }
-    
-    public static get instance(){
-        return this._instance||(this._instance=new this());
-    }
- 
-    private escucharSockets(){
-        console.log('Escuchando conexiones');
-        this.io.on('connection',(cliente)=>{
-            console.log('Cliente conectado');
-            
-            socket.mensaje(cliente, this.io);
 
-            //Desconectar
-            socket.desconectar(cliente);
-        })
+    public static get instance() {
+        return this._intance || ( this._intance = new this() );
+    }
+
+
+    private escucharSockets() {
+
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connection', cliente => {
+
+            // Conectar cliente
+            socket.conectarCliente( cliente, this.io );
+
+            // Configurar usuario
+            socket.configurarUsuario( cliente, this.io );
+
+            // Obtener usuarios activos
+            socket.obtenerUsuarios( cliente, this.io );
+
+            // Mensajes
+            socket.mensaje( cliente, this.io );
+
+            // Desconectar
+            socket.desconectar( cliente, this.io );    
+            
+
+        });
+
     }
  
-    start(callback:any){
-        this.httpServer.listen(this.port,callback);
+
+    start( callback: any ) {
+
+        this.httpServer.listen( this.port, callback );
+
     }
+
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import Server from './classes/server';
-import { SERVER_PORT } from './global/enviroment';
+import { SERVER_PORT } from './global/environment';
 import router  from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
